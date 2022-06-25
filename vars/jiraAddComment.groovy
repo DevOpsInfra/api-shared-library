57bf7f551dd9ac49f4484e3ed442d64f9b3e9a6b
miguelrl1 <107950970+DevOpsInfra@users.noreply.github.com>
2022-06-24 20:09:27 -0500

    Initial Version
    Initial Version

---
+++ vars/jiraAddComment.groovy
@@ -0,0 +1,13 @@
+def call(Map config=[:]) {
+  
+  def rawBody = libraryResource 'com/devopsbox/api/jira/addComment.json'
+  def binding = [
+    key: "${config.key}",
+    body: "${config.body}"
+  ]
+  def render = renderTemplate(rawBody,binding)
+  def issueId = "${config.key}"
+
+  sh('curl -x $URL_PROXY -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/"'+issueId+'"/comment')
+  
+}
---
+++ vars/jiraTransitionIssue.groovy
@@ -0,0 +1,10 @@
+def call(Map config=[:]) {
+  def rawBody = libraryResource 'com/devopsbox/api/jira/transitionIssue.json'
+  def binding = [
+    key: "${config.key}",
+    idTransition: "${config.id}"
+  ]
+  def render = renderTemplate(rawBody,binding)
+  def issueId = "${config.key}"
+  sh('curl -x $URL_PROXY -D- -u $JIRA_CREDENTIALS -X POST --data "'+render+'" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/"'+issueId+'"/transitions')
+}
